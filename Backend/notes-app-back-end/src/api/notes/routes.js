24f9c38dea35950da918631e07a60a1058ae87e8
Jkenyut <satrianursaputro06@gmail.com>
2023-05-16 14:01:17 +0700

    update notes

---
+++ Backend/notes-app-back-end/src/api/notes/routes.js
@@ -0,0 +1,30 @@
+/* eslint-disable quotes */
+const routes = (handler) => [
+  {
+    method: "POST",
+    path: "/notes",
+    handler: handler.postNoteHandler, // postNoteHandler hanya menerima dan menyimpan "satu" note.
+  },
+  {
+    method: "GET",
+    path: "/notes",
+    handler: handler.getNotesHandler, // getNotesHandler mengembalikan "banyak" note.
+  },
+  {
+    method: "GET",
+    path: "/notes/{id}",
+    handler: handler.getNoteByIdHandler, // getNoteByIdHandler mengembalikan "satu" note.
+  },
+  {
+    method: "PUT",
+    path: "/notes/{id}",
+    handler: handler.putNoteByIdHandler, // putNoteByIdHandler hanya mengubah "satu" note.
+  },
+  {
+    method: "DELETE",
+    path: "/notes/{id}",
+    handler: handler.deleteNoteByIdHandler, // deleteNoteByIdHandler hanya menghapus "satu" note.
+  },
+];
+
+module.exports = routes;
