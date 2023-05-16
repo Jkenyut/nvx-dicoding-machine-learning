711666b7343fa69b35dc1e423e9e4e4470715caf
Jkenyut <satrianursaputro06@gmail.com>
2023-05-16 12:02:40 +0700

    pembaruan

--- Backend/notes-app-back-end/src/server.js
+++ Backend/notes-app-back-end/src/server.js
@@ -1,34 +1,10 @@
-/* eslint-disable import/no-unresolved */
-/* eslint-disable import/extensions */
-/* eslint-disable no-unused-vars */
-// mengimpor dotenv dan menjalankan konfigurasinya
-require('dotenv').config();
 const Hapi = require('@hapi/hapi');
-const Jwt = require('@hapi/jwt');
-
-// notes
-const notes = require('./api/notes');
-const NotesService = require('./services/postgres/NotesService');
-const NotesValidator = require('./validator/notes');
-
-// users
-const users = require('./api/users');
-const UsersService = require('./services/postgres/UsersService');
-const UsersValidator = require('./validator/users');
-
-// authentications
-const authentications = require('./api/authentications');
-const AuthenticationsService = require('./services/postgres/AuthenticationsService');
-const TokenManager = require('./tokenize/TokenManager');
-const AuthenticationsValidator = require('./validator/authentications');
+const routes = require('./routes');
 
 const init = async () => {
-  const notesService = new NotesService();
-  const usersService = new UsersService();
-  const authenticationsService = new AuthenticationsService();
   const server = Hapi.server({
-    port: process.env.PORT,
-    host: process.env.HOST,
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
@@ -36,55 +12,7 @@ const init = async () => {
     },
   });
 
-  // registrasi plugin eksternal
-  await server.register([
-    {
-      plugin: Jwt,
-    },
-  ]);
-
-  // mendefinisikan strategy autentikasi jwt
-  server.auth.strategy('notesapp_jwt', 'jwt', {
-    keys: process.env.ACCESS_TOKEN_KEY,
-    verify: {
-      aud: false,
-      iss: false,
-      sub: false,
-      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
-    },
-    validate: (artifacts) => ({
-      isValid: true,
-      credentials: {
-        id: artifacts.decoded.payload.id,
-      },
-    }),
-  });
-
-  await server.register([
-    {
-      plugin: notes,
-      options: {
-        service: notesService,
-        validator: NotesValidator,
-      },
-    },
-    {
-      plugin: users,
-      options: {
-        service: usersService,
-        validator: UsersValidator,
-      },
-    },
-    {
-      plugin: authentications,
-      options: {
-        authenticationsService,
-        usersService,
-        tokenManager: TokenManager,
-        validator: AuthenticationsValidator,
-      },
-    },
-  ]);
+  server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
