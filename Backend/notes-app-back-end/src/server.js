cd9af932ea2e91dd427585f511b70bb89e01a997
Jkenyut <satrianursaputro06@gmail.com>
2023-05-17 21:53:20 +0700

    update database and openmusic new

--- Backend/notes-app-back-end/src/server.js
+++ Backend/notes-app-back-end/src/server.js
@@ -1,17 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable quotes */
+require("dotenv").config();
 const Hapi = require("@hapi/hapi");
 
 const notes = require("./api/notes");
-const NotesService = require("./services/inMemory/NotesService");
+// const NotesService = require("./services/inMemory/NotesService");
+const NotesService = require("./services/postgres/NoteService");
 const NotesValidator = require("./validator/notes");
 
-console.log(process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0");
+console.log(process.env.HOST, process.env.PORT);
 const init = async () => {
   const notesService = new NotesService();
 
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ["*"],
