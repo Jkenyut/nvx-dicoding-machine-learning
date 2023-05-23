9a1b353c55ead31906772edf5bd858ea3ffeb752
Jkenyut <satrianursaputro06@gmail.com>
2023-05-23 23:11:00 +0700

    authorization

--- Backend/notes-app-back-end/src/server.js
+++ Backend/notes-app-back-end/src/server.js
@@ -1,6 +1,7 @@
 /* eslint-disable quotes */
 require("dotenv").config();
 const Hapi = require("@hapi/hapi");
+const Jwt = require("@hapi/jwt");
 
 const notes = require("./api/notes");
 // const NotesService = require("./services/inMemory/NotesService");
@@ -33,7 +34,28 @@ const init = async () => {
       },
     },
   });
-
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+  // mendefinisikan strategy otentikasi jwt
+  server.auth.strategy("notesapp_jwt", "jwt", {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
+  });
   await server.register([
     {
       plugin: notes,
@@ -59,6 +81,7 @@ const init = async () => {
       },
     },
   ]);
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
