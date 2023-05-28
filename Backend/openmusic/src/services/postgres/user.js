ed10cc585faf80e443c65a0d3b19755fb51e2d76
Jkenyut <satrianursaputro06@gmail.com>
2023-05-28 22:37:57 +0700

    push

--- Backend/openmusic/src/services/postgres/user.js
+++ Backend/openmusic/src/services/postgres/user.js
@@ -13,7 +13,32 @@ class UserService {
     this._pool = new Pool();
   }
 
+  async getUsersByUsername(username) {
+    const query = {
+      text: "SELECT id, username, fullname FROM users WHERE username LIKE $1",
+      values: [`%${username}%`],
+    };
+    const result = await this._pool.query(query);
+    if (result.lenght < 0) {
+      throw new NotFoundError("User tidak ditemukan");
+    }
+    return result.rows[0].id;
+  }
+
+  async verifyNewUsername(username) {
+    const query = {
+      text: "SELECT username FROM users WHERE username = $1",
+      values: [username],
+    };
+    const result = await this._pool.query(query);
+
+    if (result.rows.length > 0) {
+      throw new InvariantError("Gagal menambahkan user. Username sudah digunakan.");
+    }
+  }
+
   async addUser({ username, password, fullname }) {
+    await this.verifyNewUsername(username);
     const id = `user-${nanoid(16)}`;
     const hassPassword = await bcrypt.hash(password, 10);
     const query = {
