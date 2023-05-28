bbfe9066511746f994a218e1373da60d0cf0ac1e
Jkenyut <satrianursaputro06@gmail.com>
2023-05-28 22:07:47 +0700

    feat : user schema joi

--- Backend/openmusic/src/validation/index.js
+++ Backend/openmusic/src/validation/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable quotes */
-const { albumSchema, songSchema } = require("./schema");
+const { albumSchema, songSchema, userSchema } = require("./schema");
 const InvariantError = require("../exceptions/InvariantError");
 
 const MusicValidator = {
@@ -12,6 +12,13 @@ const MusicValidator = {
   },
   validateSongPayload: (payload) => {
     const validationResult = songSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+    return validationResult.value;
+  },
+  validateUserPayload: (payload) => {
+    const validationResult = userSchema.validate(payload);
     if (validationResult.error) {
       throw new InvariantError(validationResult.error.message);
     }
