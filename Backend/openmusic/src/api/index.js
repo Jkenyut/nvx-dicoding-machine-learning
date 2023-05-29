15a93c063b31d0f6162290f1c7d733d21508994d
Jkenyut <satrianursaputro06@gmail.com>
2023-05-29 09:16:37 +0700

    fix : api index add route users

--- Backend/openmusic/src/api/index.js
+++ Backend/openmusic/src/api/index.js
@@ -2,10 +2,13 @@
 
 const AlbumHandler = require("./album/handler");
 const SongHandler = require("./song/handler");
+const UsersHandler = require("./users/handler");
 const AlbumsService = require("../services/postgres/albums");
 const SongsService = require("../services/postgres/songs");
+const usersService = require("../services/postgres/user");
 const routesAlbum = require("./album/routes");
 const routesSong = require("./song/routes");
+const routeUser = require("./users/routes");
 
 module.exports = {
   name: "OpenMusic",
@@ -15,7 +18,9 @@ module.exports = {
     const songsService = new SongsService();
     const albumsHandler = new AlbumHandler(albumsService, validator);
     const songsHandler = new SongHandler(songsService, validator);
+    const usersHandler = new UsersHandler(usersService, validator);
     server.route(routesAlbum(albumsHandler));
     server.route(routesSong(songsHandler));
+    server.route(routeUser(usersHandler));
   },
 };
