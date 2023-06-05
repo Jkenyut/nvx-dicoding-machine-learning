a1667857e3bcd026a2e876e4dfdb4a0ed282f43d
Jkenyut <satrianursaputro06@gmail.com>
2023-06-05 09:16:20 +0700

    feat : aurhorized in route user, song , and album

--- Backend/openmusic/src/api/album/routes.js
+++ Backend/openmusic/src/api/album/routes.js
@@ -3,22 +3,34 @@ const routesAlbum = (handler) => [
   {
     method: "POST",
     path: "/albums",
-    handler: handler.postAlbumHandler, // save
+    handler: handler.postAlbumHandler,
+    options: {
+      auth: "notesapp_jwt",
+    }, // save
   },
   {
     method: "GET",
     path: "/albums/{id}",
     handler: handler.getAlbumByIdHandler, // getbyid
+    options: {
+      auth: "notesapp_jwt",
+    },
   },
   {
     method: "PUT",
     path: "/albums/{id}",
     handler: handler.putAlbumByIdHandler, // edit by id
+    options: {
+      auth: "notesapp_jwt",
+    },
   },
   {
     method: "DELETE",
     path: "/albums/{id}",
     handler: handler.deleteAlbumByIdHandler, // delete by id
+    options: {
+      auth: "notesapp_jwt",
+    },
   },
 ];
 
