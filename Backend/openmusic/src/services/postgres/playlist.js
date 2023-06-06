5eb7dd0e5408691fe4a3b22e55586a208764cbaa
Jkenyut <satrianursaputro06@gmail.com>
2023-06-06 13:46:36 +0700

    feat : addplaylist service

---
+++ Backend/openmusic/src/services/postgres/playlist.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-underscore-dangle */
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+
+class PlaylistService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addPlaylist(name, owner) {
+    const id = `playlist-${nanoid()}`;
+    const query = {
+      text: 'INSERT INTO playlists VALUES($1, $2) RETURNING id',
+      values: [id, owner],
+    };
+    const result = await this._pool.query(query);
+    if (!result.rows[0].id) {
+      throw new InvariantError('Playlist gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+}
+
+module.exports = PlaylistService;
