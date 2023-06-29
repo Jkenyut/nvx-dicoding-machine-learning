15e472c021aae148d03276fd82bb4cf78f063767
Satria Nur Saputro <satrianursaputro06@gmail.com>
2023-06-29 22:49:48 +0700

    feat : comment

--- Backend/openmusic/src/tokenize/TokenManager.js
+++ Backend/openmusic/src/tokenize/TokenManager.js
@@ -1,6 +1,7 @@
 const Jwt = require('@hapi/jwt');
 const InvariantError = require('../exceptions/InvariantError');
 
+// token manager
 const TokenManager = {
   generateAccessToken: (payload) => Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY),
   generateRefreshToken: (payload) => Jwt.token.generate(payload, process.env.REFRESH_TOKEN_KEY),
