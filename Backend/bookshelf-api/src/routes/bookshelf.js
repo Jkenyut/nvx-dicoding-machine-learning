fe7686437d9cbb1f47f8ed0b8597ddd11559098f
satria nur saputro <satrianursaputro06@gmail.com>
2024-02-06 10:08:20 +0700

    bookshelf update

--- Backend/bookshelf-api/src/routes/bookshelf.js
+++ Backend/bookshelf-api/src/routes/bookshelf.js
@@ -1,20 +1,18 @@
 const {
-  addBookHandler,
-  getAllBookHandler,
-  getBookbyIdHandler,
-  editBookByIdHandler,
-  deleteBookByIdHandler,
+    addBookHandler,
+    getAllBookHandler,
+    getBookbyIdHandler,
+    editBookByIdHandler,
+    deleteBookByIdHandler,
+    helloHandler
 } = require("../controllers/bookshelf");
 const routes = [
-  {
-    method: "POST",
-    path: "/books",
-    handler: addBookHandler,
-  },
-  { method: "GET", path: "/books", handler: getAllBookHandler },
-  { method: "GET", path: "/books/{bookId}", handler: getBookbyIdHandler },
-  { method: "PUT", path: "/books/{bookId}", handler: editBookByIdHandler },
-  { method: "DELETE", path: "/books/{bookId}", handler: deleteBookByIdHandler },
+    {method: "POST", path: "/books", handler: addBookHandler},
+    {method: "GET", path: "/books", handler: getAllBookHandler},
+    {method: "GET", path: "/books/{bookId}", handler: getBookbyIdHandler},
+    {method: "PUT", path: "/books/{bookId}", handler: editBookByIdHandler},
+    {method: "DELETE", path: "/books/{bookId}", handler: deleteBookByIdHandler},
+    {method: "GET",path:"/",handler:helloHandler}
 ];
 
 module.exports = routes;
